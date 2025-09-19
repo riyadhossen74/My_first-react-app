96d52394aad0ceae0f5f6f055aa5b51bf08de537
riyadhossen74 <riyadhossen696969@gmail.com>
2025-09-19 10:27:31 +0600

    comit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Contries from './assets/componets/contries/contries'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Contries></Contries>
+      
+    </>
+  )
+}
+
+export default App
